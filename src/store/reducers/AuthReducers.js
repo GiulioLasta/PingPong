fa8fb0aa1b22383de24447c4b6794124f1f485f9
GiulioLasta <giuliolasta@gmail.com>
2021-02-14 14:37:10 +0100

    corrected payLoad with capital letters

--- src/store/reducers/AuthReducers.js
+++ src/store/reducers/AuthReducers.js
@@ -1,9 +1,10 @@
 const AuthReducer = (state = {}, actions) => {
+    debugger;
     switch(actions.type){
         case 'value': 
             return state;
         case 'SET_LOGIN': 
-            return { ...state, loggedIn: true, user: actions.payload};
+            return { ...state, loggedIn: true, user: actions.payLoad};
         case 'SET_LOGOUT': 
             return { ...state, loggedIn: false, user: {}};
         default: 
